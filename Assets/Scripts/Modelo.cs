6f1214dd55eda2267e603e11e023ff28a713af4f
Jafet <andresjeyminez@gmail.com>
2020-06-09 09:54:53 -0500

    Modelo

---
+++ Assets/Scripts/Modelo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modelo : MonoBehaviour
+{
+    public string texto1;
+    public string texto2;
+    public bool bol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
