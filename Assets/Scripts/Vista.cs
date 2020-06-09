af97252380006beb135d19dcf000dab5dbe33ab0
sebastianllerenacruz <sebastianllerenacruz@gmail.com>
2020-06-09 11:36:35 -0500

    subiendo vista

---
+++ Assets/Scripts/Vista.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vista : Modelo
+{
+    public Text textorespuesta;
+    public GameObject cuadro;
+    public GameObject Botton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cuadro.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Showtext() 
+        {
+        textorespuesta.text = texto1;
+        cuadro.SetActive(true);
+        Botton.SetActive(false);
+            
+    }
+    public void Closetext () {
+        cuadro.SetActive(false);
+        Botton.SetActive(true);
+    }
+}
