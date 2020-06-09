fcb5fa84a9192373c029c89ac7800c363b236b6e
sebastianllerenacruz <sebastianllerenacruz@gmail.com>
2020-06-09 12:33:11 -0500

    modificacion del script

--- Assets/Scripts/Vista.cs
+++ Assets/Scripts/Vista.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Vista : Modelo
+public class Vista : Controlador
 {
     public Text textorespuesta;
     public GameObject cuadro;
@@ -11,24 +11,24 @@ public class Vista : Modelo
     // Start is called before the first frame update
     void Start()
     {
-        cuadro.SetActive(false);
+        //cuadro.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textorespuesta.text =GameObject.Find("BotonParaMostrar").GetComponent<Controlador>().actualText;
     }
-    public void Showtext() 
-        {
-        textorespuesta.text = texto1;
-        cuadro.SetActive(true);
-        Botton.SetActive(false);
+    //public void Showtext() 
+        //{
+        //textorespuesta.text = actualText;
+        //cuadro.SetActive(true);
+        //Botton.SetActive(false);
             
-    }
-    public void Closetext () {
-        cuadro.SetActive(false);
-        Botton.SetActive(true);
-    }
+    //}
+   // public void Closetext () {
+        //cuadro.SetActive(false);
+        //Botton.SetActive(true);
+    //}
 }
